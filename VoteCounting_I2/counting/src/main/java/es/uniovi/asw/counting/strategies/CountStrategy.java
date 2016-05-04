028e54ae8aa435353681d9f8be78619471b7f61a
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2016-05-04 03:59:33 +0200

    Replaced folders by submodules

--- VoteCounting_I2/counting/src/main/java/es/uniovi/asw/counting/strategies/CountStrategy.java
+++
@@ -1,5 +0,0 @@
-package es.uniovi.asw.counting.strategies;
-
-public interface CountStrategy {
-    int getValue(String line, int accumulator);
-}
--- VoteCounting_I2/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sh reset.sh
-mvn clean
-mvn install -DskipTests
-mvn test
-cd tests
-mvn test -Dintegrate=true
